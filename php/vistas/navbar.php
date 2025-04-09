d37fde319f215ac7ceec37dfe47b0fd2524fe31c
CesarOcam <cesar_po21@hotmail.es>
2025-04-09 16:44:36 -0600

    Primer commit - subida de la app web

---
+++ php/vistas/navbar.php
@@ -0,0 +1,90 @@
+
+<nav class="navbar bg-body-secondary">
+        <div class="container-fluid">
+          <a class="navbar-brand m-1" href="inicio.php">AMEXPORT</a>
+        </div>
+      </nav>
+
+      <nav class="navbar bg-body-light navbar-expand-lg shadow-bottom">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse small" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item position-relative">
+                  <a class="nav-link dropdown-toggle" href="trafico" role="button" data-bs-toggle="dropdown" aria-expanded="true">Tráfico</a>
+                  <ul class="dropdown-menu border shadow">
+                    <li><a class="dropdown-item small" href="trafico">Referencias</a></li>
+                    <li><a class="dropdown-item small" href="trafico">Solicitud de Pago</a></li>
+                    <li><a class="dropdown-item small" href="trafico">Realizar Pago</a></li>
+                    <li><a class="dropdown-item small" href="trafico">Solicitud de Anticipo</a></li>
+                    <hr>
+                    <li><a class="dropdown-item small" href="trafico">Registro Cuota</a></li>
+                    <li><a class="dropdown-item small" href="trafico">Registro Facturas</a></li>
+                    <hr>
+                    <li><a class="dropdown-item small" href="trafico">Generar Poliza</a></li>
+                  </ul>
+                </li>
+                <li class="nav-item position-relative">
+                    <a class="nav-link dropdown-toggle" href="cat" role="button" data-bs-toggle="dropdown" aria-expanded="true">Catálogos</a>
+                    <ul class="dropdown-menu border shadow">
+                      <li><a class="dropdown-item small" href="cat_Clientes.php">Clientes</a></li>
+                      <li><a class="dropdown-item small" href="cat">Solicitud de Pago</a></li>
+                      <li><a class="dropdown-item small" href="cat">Realizar Pago</a></li>
+                      <li><a class="dropdown-item small" href="cat">Solicitud de Anticipo</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="cat">Registro Cuota</a></li>
+                      <li><a class="dropdown-item small" href="cat">Registro Facturas</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="cat">Generar Poliza</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item position-relative">
+                    <a class="nav-link dropdown-toggle" href="cons" role="button" data-bs-toggle="dropdown" aria-expanded="false">Consultas</a>
+                    <ul class="dropdown-menu border shadow">
+                      <li><a class="dropdown-item small" href="cons">Referencias</a></li>
+                      <li><a class="dropdown-item small" href="cons">Solicitud de Pago</a></li>
+                      <li><a class="dropdown-item small" href="cons">Realizar Pago</a></li>
+                      <li><a class="dropdown-item small" href="cons">Solicitud de Anticipo</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="cons">Registro Cuota</a></li>
+                      <li><a class="dropdown-item small" href="cons">Registro Facturas</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="cons">Generar Poliza</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item position-relative">
+                    <a class="nav-link dropdown-toggle" href="repo" role="button" data-bs-toggle="dropdown" aria-expanded="false">Reportes</a>
+                    <ul class="dropdown-menu border shadow">
+                      <li><a class="dropdown-item small" href="repo">Referencias</a></li>
+                      <li><a class="dropdown-item small" href="repo">Solicitud de Pago</a></li>
+                      <li><a class="dropdown-item small" href="repo">Realizar Pago</a></li>
+                      <li><a class="dropdown-item small" href="repo">Solicitud de Anticipo</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="repo">Registro Cuota</a></li>
+                      <li><a class="dropdown-item small" href="repo">Registro Facturas</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="#">Generar Poliza</a></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item position-relative">
+                    <a class="nav-link dropdown-toggle" href="seg" role="button" data-bs-toggle="dropdown" aria-expanded="false">Seguridad</a>
+                    <ul class="dropdown-menu border shadow">
+                      <li><a class="dropdown-item small" href="seg">Referencias</a></li>
+                      <li><a class="dropdown-item small" href="seg">Solicitud de Pago</a></li>
+                      <li><a class="dropdown-item small" href="seg">Realizar Pago</a></li>
+                      <li><a class="dropdown-item small" href="seg">Solicitud de Anticipo</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="seg">Registro Cuota</a></li>
+                      <li><a class="dropdown-item small" href="seg">Registro Facturas</a></li>
+                      <hr>
+                      <li><a class="dropdown-item small" href="seg">Generar Poliza</a></li>
+                    </ul>
+                  </li>
+              </ul>
+            </div>
+
+          </div>
+      </nav>
