e96e69da09d599703fdb6ad53cc66b916a5e0c87
CesarOcam <cesar_po21@hotmail.es>
2025-05-30 09:33:13 -0600

    Se añadio navbar a olicitudes

--- php/vistas/trafico/solicitud_pago.php
+++ php/vistas/trafico/solicitud_pago.php
@@ -80,7 +80,9 @@ $beneficiario = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </head>
 
 <?php
-include($_SERVER['DOCUMENT_ROOT'] . '/portal_web/Contabilidad/php/vistas/navbar.php');
+include_once __DIR__ . '/../../../config.php';
+
+include($_SERVER['DOCUMENT_ROOT'] . $base_url . '/php/vistas/navbar.php');
 ?>
 
 <div class="container-fluid">
