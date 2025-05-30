c37f0d1352e2e4a47d3bd9b1857dfa129f9e3eb5
CesarOcam <cesar_po21@hotmail.es>
2025-05-30 09:31:49 -0600

    Se añadio navbar a olicitudes

--- php/vistas/trafico/realizar_pago.php
+++ php/vistas/trafico/realizar_pago.php
@@ -39,9 +39,7 @@ include_once('../../modulos/conexion.php'); // Ajusta el path según sea necesar
 </head>
 
 <?php
-include_once __DIR__ . '/../../../config.php';
-
-include($_SERVER['DOCUMENT_ROOT'] . $base_url . '/php/vistas/navbar.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/portal_web/Contabilidad/php/vistas/navbar.php');
 ?>
 
 <div class="container-fluid">
